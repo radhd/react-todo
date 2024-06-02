c142f942ad8708e28cca49584390a1bbdc5de89a
Nika M. <merabishvilinikusha@gmail.com>
2024-06-02 13:43:02 +0400

    Set up the project architecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import TodoCard from "./components/TodoCard/TodoCard";
+import styles from "./App.module.css";
+
 function App() {
   return (
-    <div>
-      <h1>h1</h1>
+    <div className={styles.app}>
+      <TodoCard />
     </div>
   );
 }
---
+++ src/components/TodoCard/TodoCard.jsx
@@ -0,0 +1,11 @@
+import styles from "./TodoCard.module.css";
+
+function TodoCard() {
+  return (
+    <div className={styles.cardContainer}>
+      <p>Todo card</p>
+    </div>
+  );
+}
+
+export default TodoCard;
