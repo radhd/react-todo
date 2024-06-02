e75b57095e409e13325b2b59577ae7fc67975e41
Nika M. <merabishvilinikusha@gmail.com>
2024-06-02 14:23:41 +0400

    Set input to log an input

--- src/components/TodoCard/TodoCard.jsx
+++ src/components/TodoCard/TodoCard.jsx
@@ -1,9 +1,21 @@
 import styles from "./TodoCard.module.css";
+import { useState } from "react";
 
 function TodoCard() {
+  const [userInput, setUserInput] = useState("");
+
+  const handleChange = (event) => {
+    setUserInput(event.target.value);
+    console.log(userInput);
+  };
+
   return (
     <div className={styles.cardContainer}>
-      <p>Todo card</p>
+      <form>
+        <input type="text" onChange={handleChange} value={userInput} />
+        <button>+</button>
+      </form>
+      <div></div>
     </div>
   );
 }
